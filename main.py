c43e95f82e2e3ced1ee48686980ce5262bdfd2df
eant991 <eant991@gmail.com>
2026-06-21 15:52:57 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()  # Вот эта строка создает объект "app"
+
+quotes = [
+    "Дорогу осилит идущий.",
+    "Программирование — это искусство.",
+    "Код должен быть чистым, как слеза младенца."
+]
+
+@app.get("/")
+def read_root():
+    return {"message": "Привет! Это мой тестовый сервер."}
+
+@app.get("/quote")
+def get_random_quote():
+    return {"quote": random.choice(quotes)}
